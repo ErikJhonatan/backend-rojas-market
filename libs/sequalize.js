0b0b51bdaf8472fe42a797f098c8c3c8e94d9919
marcaloayzae <marcaloayzae@gmail.com>
2025-03-08 00:39:36 +0000

    Correctly configure my project's sequalize

---
+++ libs/sequalize.js
@@ -0,0 +1,15 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:3321035341.
+const {config} = require('../config/config');
+const { Sequelize } = require('sequelize');
+
+const USER = encodeURIComponent(config.user_db);
+const PASSWORD = encodeURIComponent(config.password_db);
+
+const URI = `postgres://${USER}:${PASSWORD}@${config.host_db}:${config.port_db}/${config.name_db}`;
+
+const sequelize = new Sequelize(URI, {
+    dialect:'postgres',
+    }
+);
+
+module.exports = sequelize;

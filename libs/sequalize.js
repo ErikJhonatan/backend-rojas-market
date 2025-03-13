985827f898a040f84db2cd5fa0992eb2eb14e5f4
erikjhonatan <marcaloayzae@gmail.com>
2025-03-12 22:03:09 -0500

    Add user model and configure Sequelize to connect to MySQL

--- libs/sequalize.js
+++ libs/sequalize.js
@@ -1,13 +1,19 @@
-const { Sequelize } = require('@sequelize/core');
-const { MySqlDialect } = require('@sequelize/mysql');
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+const setupModels = require('../db/models');
 
-const sequelize = new Sequelize({
-  dialect: MySqlDialect,
-  database: process.env.DB_NAME || 'my-store-api',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3306,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME|| 'my_store_data',
+  process.env.DB_USER || 'root',
+  process.env.DB_PASSWORD || '',
+  {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+    logging: true,
+  }
+);
+
+setupModels(sequelize);
 
 module.exports = sequelize;

7f2cd0d5082d3f1fa8b9da32e0900612f9d09388
erikjhonatan <marcaloayzae@gmail.com>
2025-03-16 15:25:36 -0500

    Update Sequelize for MySQL configuration and add migration for the 'role' column in the user model

--- db/config.js
+++ db/config.js
@@ -2,7 +2,11 @@ const { config } = require('../config/config')
 
 module.exports = {
   development: {
-    ...config
+    "username": "root",
+    "password": null,
+    "database": "store_bd",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
   production: {
     ...config

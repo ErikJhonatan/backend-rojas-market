2ea2dd60b094a4d470908804bab1d6fb7052d651
erikjhonatan <marcaloayzae@gmail.com>
2025-04-15 19:43:37 -0500

    Add validation schemes for orders and products, including limits and offsets in the product query.

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -3,9 +3,13 @@ const Joi = require('joi');
 const id = Joi.string().uuid();
 const name = Joi.string().min(3).max(15);
 const price = Joi.number().integer().min(10);
+const price_min = Joi.number().integer().min(10);
+const price_max = Joi.number().integer().min(10);
 const description = Joi.string().min(10).max(100);
-const image = Joi.string().uri();
 const categoryId = Joi.number().integer().min(1);
+const image = Joi.string().uri();
+const limit = Joi.number().integer().min(1).max(50);
+const offset = Joi.number().integer().min(0);
 
 const createProductSchema = Joi.object({
   name: name.required(),
@@ -27,4 +31,15 @@ const getProductSchema = Joi.object({
   id: id.required(),
 });
 
-module.exports = { createProductSchema, updateProductSchema, getProductSchema }
+const queryProductSchema = Joi.object({
+  limit: limit,
+  offset: offset,
+  price: price,
+  price_min: price_min,
+  price_max: price_max.when('price_min', {
+    is: Joi.number().integer().min(10),
+    then: Joi.required()
+  })
+})
+
+module.exports = { createProductSchema, updateProductSchema, getProductSchema, queryProductSchema };
